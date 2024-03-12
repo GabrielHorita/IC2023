bfd9cee1e1c081468071570933cd7a6509050274
GabrielHorita <126568494+GabrielHorita@users.noreply.github.com>
2024-03-12 20:40:49 -0300

    Add files via upload

---
+++ SessaoChoque/Graf8_linhacontinua.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+
+x = []
+y = []
+
+with open("Graf8L.txt", "r") as arquivo:
+    linhas = arquivo.readlines()
+    for linha in linhas[1:]:
+        if not linha.startswith('#'):
+            valores = linha.strip().split()
+            if len(valores) == 2:
+                x_original = float(valores[0])
+                y_original = float(valores[1])
+
+                x_adaptado = (x_original / 338) * 3
+                y_adaptado = (y_original / 338) * 7 - 1
+
+                x.append(x_adaptado)
+                y.append(y_adaptado)
+
+x_smooth = np.linspace(min(x), max(x), 100)
+
+f = interp1d(x, y, kind='cubic')
+def calcular_valor_de_y(valor_de_x):
+    return f(valor_de_x)
+
+def sigmacc(valor_de_x):
+    return np.log10((10**4.5)*np.sqrt((valor_de_x +1)**2 -1)*(valor_de_x + 1))
+
